ae4f6b704cf0d86f7ecd18fb48d086e595182b22
Raunak Biswas <pbraunak2016@gmail.com>
2026-05-01 00:54:24 +0530

    feat: UI changes

--- src/Routes/student_login.jsx
+++ src/Routes/student_login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { ArrowLeftToLine } from "lucide-react";
+import { ArrowLeftToLine,GraduationCap } from "lucide-react";
 import { apiCall, API_ENDPOINTS } from "../config/api";
 
 export default function LoginPage() {
@@ -71,16 +71,19 @@ export default function LoginPage() {
             <div className="w-4/7 flex items-center justify-center">
                 <div className="w-full max-w-md p-8">
                     {/* Logo + Title */}
-                    <div className="flex justify-center gap-2 mb-3">
-                        <img className='w-10 pointer-events-none' src="logo.png" alt="" />
-                        <h1 className="text-3xl font-bold text-center mb-2">EduShield</h1>
+                    <div className="flex flex-col items-center gap-2 mb-3">
+                        <img className='size-20 pointer-events-none' src="logo.png" alt="" />
+                        <div className="flex flex-row items-center gap-5">
+                <GraduationCap />
+                <h1 className="text-2xl sm:text-3xl font-bold mb-2">Student Log In</h1>
+                </div>
                     </div>
                     <p className="text-sm text-gray-500 text-center mb-6">
                         Learn, prepare, and stay safe — turning disaster readiness into a smart, engaging journey
                     </p>
 
                     {/* Role Selection */}
-                    <div className="flex justify-center space-x-6 mb-6">
+                    {/* <div className="flex justify-center space-x-6 mb-6">
                         <label className="flex items-center space-x-2">
                             <input
                                 type="radio"
@@ -105,7 +108,7 @@ export default function LoginPage() {
                             />
                             <span>School</span>
                         </label>
-                    </div>
+                    </div> */}
 
                     {/* Error Message */}
                     {error && (
